632623822f9c06aa7bf23c7641df0be294c73d67
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-08-10 20:36:08 +0300

    refactor:use AUTH_USER_MODEL  to support the custom user model, and add the initial chat migration.

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
 import shortuuid
+
+User = settings.AUTH_USER_MODEL
 
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=128, unique=True, blank=True)

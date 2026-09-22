d2960d70e4038ad2a7c00f9a798a71348bf7711c
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-09-22 21:14:13 +0300

    feat: implement chat view to retrieve and display messages from the selected chat group

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render 
+from django.shortcuts import render, get_object_or_404, redirect 
 from django.contrib.auth.decorators import login_required
+from .models import *
 
 @login_required
 def chat_view(request, chatroom_name='public-chat'):
-    return render(request, 'chat/chat.html')
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+    chat_messages = chat_group.chat_messages.all()[:30]
+    return render(request, 'chat/chat.html',{'chat_messages': chat_messages})

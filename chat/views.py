9e9bc75b46f25b0b04bd6b64a1ebbab956b5ae75
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-09-22 23:35:17 +0300

    feat: enhance chat_view to handle message creation via HTMX

--- chat/views.py
+++ chat/views.py
@@ -6,4 +6,16 @@ from .models import *
 def chat_view(request, chatroom_name='public-chat'):
     chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
     chat_messages = chat_group.chat_messages.all()[:30]
-    return render(request, 'chat/chat.html',{'chat_messages': chat_messages})
+    if request.htmx:
+        form = ChatmessageCreateForm(request.POST)
+        if form.is_valid:
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            context = {
+                'message' : message,
+                'user' : request.user
+            }
+            return render(request, 'chat/partials/chat_message_p.html', context)
+    return render(request, 'chat/chat.html',{'chat_messages': chat_messages , 'form' : form})
